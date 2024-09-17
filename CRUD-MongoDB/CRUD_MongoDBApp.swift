447fb0f069d61a3165abc3a4e338c3084ff33e6f
Benjamin <benjaminbelloeil03@gmail.com>
2024-09-17 14:15:54 -0600

    Initial Commit

---
+++ CRUD-MongoDB/CRUD_MongoDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_MongoDBApp.swift
+//  CRUD-MongoDB
+//
+//  Created by Benjamin Belloeil on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_MongoDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
